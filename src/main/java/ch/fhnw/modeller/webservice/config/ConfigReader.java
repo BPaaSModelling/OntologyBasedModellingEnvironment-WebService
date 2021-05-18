5d586ad9c6acd42aed918e6f9248c3066a217cfa
ma1163147 <ma1163147@OL19AM00306.adm.ds.fhnw.ch>
2021-05-18 15:52:46 +0200

    Read fuseki triplestore endpoint from config file.

---
+++ src/main/java/ch/fhnw/modeller/webservice/config/ConfigReader.java
@@ -0,0 +1,107 @@
+package ch.fhnw.modeller.webservice.config;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConfigReader {
+
+	private static ConfigReader instance;
+
+	/**
+	 * Lines that start with COMMENT_PREFIX will be treated as comments and ignored.
+	 */
+	private static final String COMMENT_PREFIX = "#";
+
+	private String configFilePath = "config.txt";
+
+	private Map<String, String> configEntries;
+
+	public ConfigReader() {
+
+		this.configEntries = new HashMap<>();
+
+		readConfigFile();
+
+	}
+
+	/**
+	 * Returns the config entry with the given name.
+	 * 
+	 * If this value does not exist or if it could not be read from a config file
+	 * then this method will return the defaultValue.
+	 * 
+	 * @param name         the name of the entry that should be returned
+	 * @param defaultValue a default value to return if the entry does not exist
+	 * @return a String with the value of the entry
+	 */
+	public String getEntry(String name, String defaultValue) {
+
+		if (this.configEntries.containsKey(name)) {
+			return this.configEntries.get(name);
+		} else {
+			System.err.println("Config file entry \"" + name + "\" could not be found. Using default value of \""
+					+ defaultValue + "\"");
+
+			return defaultValue;
+		}
+
+	}
+
+	private void readConfigFile() {
+
+		this.configEntries = new HashMap<>();
+
+		File file = new File(this.configFilePath);
+
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+
+			br.lines().filter(line -> !line.isBlank()).filter(line -> !line.startsWith(ConfigReader.COMMENT_PREFIX))
+					.forEach(this::readLine);
+
+		} catch (IOException e) {
+			System.err.println();
+			System.err.println("Failed to read config file \"" + configFilePath + "\":");
+			e.printStackTrace();
+		}
+	}
+
+	private void readLine(String line) {
+		String[] splitLine = line.split("=");
+		if (splitLine.length < 2) {
+			System.err.println();
+			System.err.println("The config file contains an invalid line: \"" + line + "\" This line will be ignored.");
+			return;
+		}
+
+		String key = splitLine[0];
+
+		String value = "";
+
+		for (int i = 1; i < splitLine.length; i++) {
+			value += splitLine[i];
+		}
+
+		if (this.configEntries.containsKey(key)) {
+			System.err.println();
+			System.err.println("The config file contains duplicate entries with name \"" + key
+					+ "\". The second occurence will be ignored.");
+			return;
+		}
+
+		this.configEntries.put(key, value);
+	}
+
+	public static ConfigReader getInstance() {
+
+		if (instance == null) {
+			instance = new ConfigReader();
+		}
+
+		return instance;
+
+	}
+}
--- src/main/java/ch/fhnw/modeller/webservice/ontology/OntologyManager.java
+++ src/main/java/ch/fhnw/modeller/webservice/ontology/OntologyManager.java
@@ -13,7 +13,6 @@ import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
-import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.Lang;
@@ -24,6 +23,8 @@ import org.apache.jena.update.UpdateFactory;
 import org.apache.jena.update.UpdateProcessor;
 import org.apache.jena.update.UpdateRequest;
 
+import ch.fhnw.modeller.webservice.config.ConfigReader;
+
 
 public final class OntologyManager {
 
@@ -31,9 +32,9 @@ public final class OntologyManager {
 	private boolean localOntology = true;
 	//private Model rdfModel;
 	
-	private static String TRIPLESTOREENDPOINT 	= "http://localhost:3030/modellingEnvironment"; // endpoint for standalone server
-	//private static String TRIPLESTOREENDPOINT 	= "https://fusekiherokutest.herokuapp.com/modellingEnvironment"; // endpoint for demo server on heroku
-	//private static String TRIPLESTOREENDPOINT 	= "https://aoame-fuseki.herokuapp.com/ModEnv"; // endpoint for deployment
+	// Read the endpoint from a config file. If it does not exist use localhost.
+	private static String TRIPLESTOREENDPOINT 	= ConfigReader.getInstance().getEntry("triplestoreEndpoint", "http://localhost:3030/modellingEnvironment"); 
+
 	private static String UPDATEENDPOINT 		= TRIPLESTOREENDPOINT + "/update";
 	private static String QUERYENDPOINT			= TRIPLESTOREENDPOINT + "/query";
 	private static String READENDPOINT			= TRIPLESTOREENDPOINT + "/get";
