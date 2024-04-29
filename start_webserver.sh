abcc5bf3eb9f73ca6cc13a8e2f1005397ef06180
Kiril <kirilbuga@yahoo.com>
2024-04-29 18:36:27 +0200

    Add script to start webserver for Linux and Mac
    The new script, start_webserver.sh, has been added to automate the process of starting the webserver. It navigates to the necessary directory, runs the webapp-runner.jar, and assigns execution permissions to runJava.sh.

--- start_webserver.sh
+++ start_webserver.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Navigate to the directory containing the jar and war files if necessary
-# cd /path/to/files
+#If this script doesn't work, try to give it the right permissions like follows:
+#cd /path/to/your/script
+#chmod +x start_webserver.sh
+#./start_webserver.sh
 
-java -jar -Ddebug=true ./target/dependency/webapp-runner.jar ./target/OntologyBasedModellingEnvironment-WebService-0.0.1-SNAPSHOT.war -AmaxHttpHeaderSize="65536"
-chmod +x runJava.sh
+
+java -jar -debug ./target/dependency/webapp-runner.jar ./target/OntologyBasedModellingEnvironment-WebService-0.0.1-SNAPSHOT.war -AmaxHttpHeaderSize=65536
