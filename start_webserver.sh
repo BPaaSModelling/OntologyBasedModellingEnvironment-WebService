deb1ecb07373daab2a232682dddb11b7a4f55c1f
Kiril <kirilbuga@yahoo.com>
2024-04-29 19:07:01 +0200

    Add script to start webserver for Linux and Mac

--- start_webserver.sh
+++ start_webserver.sh
@@ -5,5 +5,4 @@
 #chmod +x start_webserver.sh
 #./start_webserver.sh
 
-
 java -jar -debug ./target/dependency/webapp-runner.jar ./target/OntologyBasedModellingEnvironment-WebService-0.0.1-SNAPSHOT.war -AmaxHttpHeaderSize=65536
