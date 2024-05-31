1fc5cb05d0b2f877210484a48d065d802297e581
Kiril <kirilbuga@yahoo.com>
2024-05-31 10:51:54 +0200

    Renamed CookieRequestFilter to JWTAuthFilter and updated logic
    The class 'CookieRequestFilter' has been renamed to 'JWTAuthFilter' to reflect its actual purpose. It now performs JWT authentication instead of cookie-based methods. Additionally, unnecessary imports and comments have been removed, while essential documentation/comments have been added for better understanding of the code. Functionality for decoding user data and validating tokens has also been updated.

--- src/main/java/ch/fhnw/modeller/webservice/filter/JWTAuthFilter.java
+++ src/main/java/ch/fhnw/modeller/webservice/filter/JWTAuthFilter.java
@@ -1,11 +1,8 @@
 package ch.fhnw.modeller.webservice.filter;
 
-import ch.fhnw.modeller.auth.AuthenticationControllerProvider;
-import ch.fhnw.modeller.auth.SessionValidationServlet;
 import ch.fhnw.modeller.auth.UserService;
 
 import ch.fhnw.modeller.webservice.ontology.OntologyManager;
-import com.auth0.AuthenticationController;
 import com.auth0.json.mgmt.users.User;
 import com.auth0.jwk.Jwk;
 import com.auth0.jwk.JwkException;
@@ -18,8 +15,6 @@ import com.auth0.jwt.interfaces.JWTVerifier;
 import com.google.gson.Gson;
 
 import javax.annotation.Priority;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
@@ -27,25 +22,31 @@ import javax.ws.rs.core.*;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.net.URL;
-import java.net.URLDecoder;
 import java.security.Principal;
 import java.security.interfaces.RSAPublicKey;
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Logger;
 import java.util.concurrent.*;
-import static ch.fhnw.modeller.auth.SessionValidationServlet.getUserData;
 
+/**
+ * JWTAuthFilter is a container request filter that performs JWT authentication.
+ * It extracts the JWT token from the request's Authorization header,
+ * validates the token using the Auth0 server, and sets the authenticated user
+ * and security context in the request context for further processing.
+ */
 @Provider
 @Priority(Priorities.AUTHENTICATION)
-public class CookieRequestFilter implements ContainerRequestFilter {
+public class JWTAuthFilter implements ContainerRequestFilter {
     private static final Logger LOGGER = Logger.getLogger("JWTAuthFilter.class.getName()");
     private static final String AUTH0_DOMAIN = "https://dev-aoame.eu.auth0.com/";
     private static final String AUDIENCE = "https://aoame-webservice";
 //    private final TokenCache tokenCache = new TokenCache(); // Cache tokens for faster validation
     private static final Gson gson = new Gson();
-
+    /**
+     * This method is called for every HTTP request to the server.
+     * It checks if the Authorization header is present and starts with "Bearer ".
+     * If the Authorization header is present, it extracts the access token from it and validates the token using the validateToken method.
+     * If the token is valid, it sets a new SecurityContext for the request with the decoded JWT and the user's email.
+     */
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
@@ -58,20 +59,15 @@ public class CookieRequestFilter implements ContainerRequestFilter {
 
         String accessToken = authorizationHeader.substring("Bearer".length()).trim();
         try {
-            // Decode user data
             User user = new User();
             user.setEmail(userEmailHeader);
-
+            // Decode user data
             DecodedJWT jwt = validateToken(accessToken);
             SecurityContext securityContext = requestContext.getSecurityContext();
             requestContext.setSecurityContext(new JWTSecurityContext(jwt, securityContext.isSecure()));
-
-//            User user = getUserData(userData);
-
-            //userService.initializeUserGraph(user.getEmail());
-            // Set Current user in the Ontology Manager for the upcoming request
             UserService userService = new UserService(user);
             requestContext.setProperty("userService", userService);
+            // Set current userService in the Ontology Manager for the upcoming request
             OntologyManager.getInstance().setUserService(requestContext);
         } catch (Exception e) {
             LOGGER.severe("Error validating token: " + e.getMessage());
@@ -91,21 +87,19 @@ public class CookieRequestFilter implements ContainerRequestFilter {
 //        user.setIss(jwt.getIssuer());
 //        user.setNickname(jwt.getClaim("nickname").asString());
 //        user.setName(jwt.getClaim("name").asString());
-////        user.setExp(jwt.getExpiresAt());
-////        user.setIat(jwt.getIssuedAt());
+//        user.setExp(jwt.getExpiresAt());
+//        user.setIat(jwt.getIssuedAt());
 //        user.setEmail(jwt.getClaim("email").asString());
 //        user.setSid(jwt.getId());
 //        user.setSid(jwt.getClaim("sid").asString());
 //
 //        return user;
 //    }
-
+    /**
+     * This method decodes the JWT, retrieves the public key from Auth0's JWKS (JSON Web Key Set) endpoint,
+     * and verifies the JWT signature with the public key. If the verification is successful, it returns the decoded JWT.
+     */
     private DecodedJWT validateToken(String token) throws JwkException, IOException {
-//        DecodedJWT jwt = tokenCache.get(token);
-//        if (jwt != null) {
-//            return jwt;
-//        }
-
         DecodedJWT jwt = JWT.decode(token);
 
         JwkProvider provider = new UrlJwkProvider(new URL(AUTH0_DOMAIN + ".well-known/jwks.json"));
@@ -124,7 +118,10 @@ public class CookieRequestFilter implements ContainerRequestFilter {
         return  jwt;
     }
 }
-
+/**
+ * TokenCache is a simple cache implementation that stores tokens and their corresponding decoded JWTs.
+ * It uses a ConcurrentHashMap for storing the tokens and a ScheduledExecutorService for removing tokens from the cache after an hour.
+ */
 class TokenCache {
     private final ConcurrentHashMap<String, DecodedJWT> cache = new ConcurrentHashMap<>();
     private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
@@ -143,7 +140,11 @@ class TokenCache {
         executorService.schedule(expirationTask, 1, TimeUnit.HOURS);
     }
 }
-
+/**
+ * JWTSecurityContext implements the SecurityContext interface.
+ * It provides methods for getting the user principal (in this case, the subject of the JWT),
+ * checking if the user is in a certain role (not implemented), checking if the request is secure, and getting the authentication scheme (returns "Bearer").
+ */
 class JWTSecurityContext implements SecurityContext {
 
     private final DecodedJWT jwt;
@@ -174,5 +175,4 @@ class JWTSecurityContext implements SecurityContext {
     public String getAuthenticationScheme() {
         return "Bearer";
     }
-
 }
